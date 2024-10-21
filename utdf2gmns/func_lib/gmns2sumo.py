db2ec021439c1671f6d9ce4b8be66180e6cb7f01
xyluo25 <luoxiangyong01@gmal.com>
2024-10-20 20:06:21 -0700

    updates

--- utdf2gmns/func_lib/gmns2sumo.py
+++ utdf2gmns/func_lib/gmns2sumo.py
@@ -7,3 +7,5 @@
 '''
 
 # SUMO network specification: https://sumo.dlr.de/docs/Networks/SUMO_Road_Networks.html#network_format
+# SUMO-GUI Specification: https://sumo.dlr.de/docs/sumo-gui.html#interaction_with_the_view
+# SUMO netedit specification: https://sumo.dlr.de/docs/Netedit/index.html#processing_menu_options
