71d2ce96f98492ba936496a4166da9ce1e222a6f
xyluo25 <luoxiangyong01@gmal.com>
2024-11-19 00:10:43 -0700

    updates

--- utdf2gmns/func_lib/gmns2sumo.py
+++ utdf2gmns/func_lib/gmns2sumo.py
@@ -9,3 +9,16 @@
 # SUMO network specification: https://sumo.dlr.de/docs/Networks/SUMO_Road_Networks.html#network_format
 # SUMO-GUI Specification: https://sumo.dlr.de/docs/sumo-gui.html#interaction_with_the_view
 # SUMO netedit specification: https://sumo.dlr.de/docs/Netedit/index.html#processing_menu_options
+
+import sumolib
+import xmltodict
+
+res = xmltodict.parse('')
+
+from sumolib.net.node import Node
+from sumolib.net.edge import Edge
+from sumolib import net
+
+path_net = r"C:\Users\xyluo25\ASU Dropbox\Xiangyong Luo\xluo25_asu\Learning\SUMO_xl\00hello\helloWorld.net.xml"
+
+ne = net.readNet(path_net)
